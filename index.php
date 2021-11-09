37b7a0b9de1a597a5c13e43816e056921f3cb1eb
scruncho <60299363+scruncho@users.noreply.github.com>
2021-11-08 22:54:55 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<?php 
+    $title = 'Index';
+    require_once 'includes/header.php';
+    
+?>
+<div>
+<center><img src="image/logo.jpeg" style="width:300px;height:400px;"></center>
+    <h1>Hello, world!</h1>
+ </div>
+<?php 
+    require_once 'includes/footer.php';
+?>
+ 
