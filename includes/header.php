bf8d232dc3782c23f29e7819bf9e6406e5391612
scruncho <60299363+scruncho@users.noreply.github.com>
2021-11-13 13:16:19 -0500

    f

--- includes/header.php
+++ includes/header.php
@@ -27,7 +27,7 @@
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
  <span class="navbar-toggler-icon"></span>
   
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <!-- <div class="collapse navbar-collapse" id="navbarNavAltMarkup"> -->
     
   
   <a href="signin.php">Sign In</a>
