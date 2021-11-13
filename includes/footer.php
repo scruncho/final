e1924266e7df4fe05ab05957150211d6991ef083
scruncho <60299363+scruncho@users.noreply.github.com>
2021-11-13 01:39:34 -0500

    p

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,14 @@
 
-
+<footer>
 
   <div id="footer">
       <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script>, TOP STAR KITCHEN</p>
 
         <?php  ?> 
-        <link rel="stylesheet" href="pagestyles.css"/>
+        <link rel="stylesheet" href="pagestyles.css">
     </div>
   </div>
+  </footer>
  <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
