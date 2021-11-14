04c9a9687292463ae12beb31df4227a8bbe97944
scruncho <60299363+scruncho@users.noreply.github.com>
2021-11-14 18:29:43 -0500

    d

--- index.php
+++ index.php
@@ -6,12 +6,17 @@
 ?>
 <div>
 <link rel="stylesheet" href="pagestyles.css">
+<div class="container p-5 my-5 border"> 
 <center><img src="image/logo.jpeg" style="width:150px;height:150px;"></center>
+</div>
 <br>
 <br>
+<p><h1> Its here for the first time when Top Star Kitchen Presents their customer appreciation day</h1></p>
+
 <!-- <center><button type="submit" name="submit" class="btn btn-success" class="btn-block">Register</button></center> -->
 <center><a href="register.php" class="btn btn-success" class="btn-block">Register</a></center> 
-<br>
+
+
 <br>
 <br>
     <!-- <?php require_once 'includes/successmsg.php'; ?> -->
